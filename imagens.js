a3a1c1564e74bb08e8558f24d45a28ce6e791e2d
Pedro Victor Barbosa Fernandes <pedro.fernandes@matriz.fornecedoramaquinas.com.br>
2022-07-01 15:50:44 -0300

    Primeiro Commit

---
+++ imagens.js
@@ -0,0 +1,16 @@
+
+
+
+let ImagemDeFundo;
+let sapinho;
+let carro1;
+let carro2;
+let carro3;
+
+function preload(){
+    ImagemDeFundo = loadImage("img/background.png");
+    sapinho = loadImage("img/sapinho.png");
+    carro1 = loadImage("img/carro1.png");
+    carro2 = loadImage("img/carro2.png");
+    carro3 = loadImage("img/carro3.png");
+  }
---
+++ sketch.js
@@ -0,0 +1,28 @@
+
+
+
+
+//carro
+
+
+//ator
+
+
+
+
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(ImagemDeFundo);
+  desenhaSapinho();
+  desenhaCarros();
+  movimentoCarros();
+  movimentoSapinho();
+}
+
+
+
+
+
